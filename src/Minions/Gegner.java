87137c75f7df0b700f466744e2408d2decb67eb1
genti <genti@10.91.60.143>
2017-03-10 09:21:28 +0100

    asdf

--- src/Minions/Gegner.java
+++ src/Minions/Gegner.java
@@ -2,9 +2,5 @@ package Minions;
 
 public class Gegner {
 
-<<<<<<< HEAD
-	private int damage; //hallo heyhi
-=======
-	private int damage; //hallo hallo
->>>>>>> branch 'master' of https://github.com/Glamdo/TowerDefend.git
+	private int damage; //hallo heyhihallo
 }
